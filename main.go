ff8c61e23ba0451a5474fdeeb4e650d848f4201b
bob3000 <mail@bob3000.net>
2019-05-16 20:33:00 +0200

    exif extraction

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/docopt/docopt-go"
+	"github.com/rwcarlsen/goexif/exif"
 )
 
 // VERSION of the program
 const VERSION = "1"
+
+// Photo is a picture of stb or sth
+type Photo struct {
+	path     string
+	exifData *exif.Exif
+}
+
+// ExifReader provides exif data from a photo
+type ExifReader interface {
+	load()
+	date() time.Time
+}
 
 func main() {
 	usage := `Photo Sort - sorts your photos
@@ -27,21 +41,27 @@ Options:
 	if err != nil {
 		log.Fatal(err)
 	}
-	fileList := make([]string, 10)
-	fileList = gatherFiles(arguments["<inputdir>"].(string), fileList, arguments["--recursive"].(bool))
-	fmt.Println(fileList)
+	var photoList []*Photo
+	photoList = gatherFiles(arguments["<inputdir>"].(string), photoList, arguments["--recursive"].(bool))
+	for _, p := range photoList {
+		p.load()
+		fmt.Println(p.date().Year())
+	}
 }
 
-func gatherFiles(path string, fileList []string, recursive bool) []string {
+func gatherFiles(path string, photoList []*Photo, recursive bool) []*Photo {
 	allowedExtensions := []string{".jpg", ".jpeg"}
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if photoList == nil {
+		photoList = make([]*Photo, 0, len(files))
+	}
 	for _, f := range files {
 		if f.IsDir() {
 			if recursive {
-				fileList = gatherFiles(fmt.Sprintf("%s/%s", path, f.Name()), fileList, recursive)
+				photoList = gatherFiles(fmt.Sprintf("%s/%s", path, f.Name()), photoList, recursive)
 			}
 			continue
 		}
@@ -54,7 +74,27 @@ func gatherFiles(path string, fileList []string, recursive bool) []string {
 		if !hasAllowedExtension {
 			continue
 		}
-		fileList = append(fileList, f.Name())
+		photoList = append(photoList, &Photo{path: fmt.Sprintf("%s/%s", path, f.Name()), exifData: nil})
 	}
-	return fileList
+	return photoList
+}
+
+func (p *Photo) load() {
+	fHandle, err := os.Open(p.path)
+	defer fHandle.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.exifData, err = exif.Decode(fHandle)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (p *Photo) date() time.Time {
+	time, err := p.exifData.DateTime()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return time
 }
